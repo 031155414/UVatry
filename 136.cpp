4600ddcb0e3d098b88489b13d78000b07af85c10
Yu-Sung,Liu <73282790+031155414@users.noreply.github.com>
2021-03-30 19:30:11 +0800

    2021/03/30

---
+++ 136.cpp
@@ -0,0 +1,38 @@
+﻿// 136.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int uglyNumber[1500] = { 1 };
+	int i2 = 0;
+	int i3 = 0;
+	int i5 = 0;
+	int temp = 0;
+	for (int i = 1; i < 1500; i++) {
+		while (uglyNumber[i2] * 2 <= uglyNumber[i - 1])
+			i2++;
+		while (uglyNumber[i3] * 3 <= uglyNumber[i - 1])
+			i3++;
+		while (uglyNumber[i5] * 5 <= uglyNumber[i - 1])
+			i5++;
+
+		temp = min(uglyNumber[i2] * 2, uglyNumber[i3] * 3);
+		temp = min(temp, uglyNumber[i5] * 5);
+		uglyNumber[i] = temp;
+	}
+	cout << "The 1500'th ugly number is " << uglyNumber[1499] << '.' << endl;
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示:
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
